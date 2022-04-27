0b057a621b8d63130b6cd73f8b045077a28564d8
Jylah <66040569+digiArchitect@users.noreply.github.com>
2022-04-27 17:28:25 +0000

    There was literally zero reason for me to remove elements if im iterating through it  lOL

--- main.py
+++ main.py
@@ -11,34 +11,27 @@ f.close()
 
   
 def generateTweet(lyrics,seen,currentCount):
-  if(len(lyrics) > 0):
-    s = lyrics[0]
-    if(s not in seenLyrics):
-      client.create_tweet(text=s)
-      seen.append(s)
-      lyrics.pop(0)
+  for x in lyrics:
+    if(x not in seen):
+      time.sleep(10800)
+      client.create_tweet(text=x)
+      seen.append(x)
       dictionary = {
       'currentCount': data['currentCount'],
       'lyrics': lyrics,
       'seenLyrics': seen
-         
+       
     }
       changeData(dictionary)
-      
-      
-    else:
-      lyrics.pop(0)
-      generateTweet(lyrics,seen,currentCount)
+
+
    
     
 
-
+generateTweet(lyrics,seen,currentCount)
 
     
-while True:
 
-  generateTweet(lyrics,seen,currentCount)
-  time.sleep(10800)
     
     
     
